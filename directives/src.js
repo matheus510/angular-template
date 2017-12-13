19ee20ecfb40215cf4c561895463a435b62989ab
Matheus <matheus510.fonseca@gmail.com>
2017-12-13 15:24:57 -0200

    added src directive

---
+++ directives/src.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function srcDirective($, data, options, angularTemplate) {
+  /**
+   * ht-src expression
+   */
+  var htSrcs = $("*[" + options.prefix + "-if]");
+  htSrcs.each(function (i, elem) {
+    var expr = $(this).attr(options.prefix + '-src').trim();
+    $(this).attr('src', expr)
+    $(this).removeAttr(options.prefix + '-if');
+  });
+}
+
+module.exports = IfDirective
